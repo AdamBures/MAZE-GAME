ab61264cb1af362abbebd0fc1a794d535f4de55e
Matthew <matej.kriz2003@gmail.com>
2021-04-21 21:11:02 +0200

    Make instances subscriptable

--- Labyrinth.py
+++ Labyrinth.py
@@ -2,3 +2,12 @@ class Labyrinth:
 
     def __init__(self, labyrinth: list[list[str]]):
         self.labyrinth = labyrinth
+
+
+    def __getitem__(self, item: int):
+        """
+        Make instance of this class to be subscriptable.
+        :param item: Position in list
+        :return: Value of object in "item" position
+        """
+        return self.labyrinth[item]
