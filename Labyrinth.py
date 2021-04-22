d34929776e1881ce6f732b5508ca6ca5b56aec38
Matthew <matej.kriz2003@gmail.com>
2021-04-22 08:19:56 +0200

    Add method __len__ Improve docstring in __init__

--- Labyrinth.py
+++ Labyrinth.py
@@ -1,20 +1,26 @@
 from typing import List
 
-class Labyrinth:
-    """
-    :param labyrinth: list of chars
-    :type labyrinth: list
-    :param index: index at which to pick a list and then pick char from *labyrinth*
-    :type index: string
-    """
-    def __init__(self, labyrinth: List[List[str]]):
-        self.labyrinth = labyrinth
 
+class Labyrinth:
+
+    def __init__(self, labyrinth: List[List[str]]):
+        """
+        :param labyrinth: list of chars
+        :type labyrinth: list
+        :param index: index at which to pick a list and then pick char from *labyrinth*
+        :type index: string
+        """
+        self.labyrinth = labyrinth
+        self.labyrinth_width = len(labyrinth[0])
+        self.labyrinth_height = len(labyrinth)
 
     def __getitem__(self, item: int):
         """
-        Make instance of this class to be subscriptable.
+        Make instance of this class subscriptable.
         :param item: Position in list
         :return: Value of object in "item" position
         """
         return self.labyrinth[item]
+
+    def __len__(self):
+        return len(self.labyrinth)
