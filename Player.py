7b7a9075bc2d2a10025f85a4b25b1ea291b1aa94
Matthew <matej.kriz2003@gmail.com>
2021-04-22 10:36:08 +0200

    Encapsulate all Player attr, Add change_position and get_position method

--- Player.py
+++ Player.py
@@ -6,9 +6,9 @@ import Labyrinth
 class Player:
 
     def __init__(self, labyrinth: Labyrinth, row_position: int, col_position: int):
-        self.row_position = row_position
-        self.col_position = col_position
-        self.labyrinth = labyrinth
+        self.__row_position = row_position
+        self.__col_position = col_position
+        self.__labyrinth = labyrinth
 
     @staticmethod
     def find_player_position(labyrinth: Labyrinth) -> Tuple[int, int]:
@@ -21,9 +21,31 @@ class Player:
             for col in range(0, len(labyrinth[0])):
                 if labyrinth[row][col] == 'S':
                     return row, col
-        
+
         # todo: handle exception, if there is no field holding 'S' then something is wrong
         return -1, -1
+
+    def get_position(self):
+        """
+        :return: Current position of a player
+        """
+        return self.__row_position, self.__col_position
+
+    def change_position(self, add_row: int = 0, add_col: int = 0) -> None:
+        """
+        Change position of the player
+        :param add_row: Row which will be added to its origin position
+        :param add_col: Col which will be added to its origin position
+        :return: None
+        """
+        next_row = self.__row_position + add_row
+        next_col = self.__col_position + add_col
+        if len(self.__labyrinth[0]) > next_row >= 0:
+            if len(self.__labyrinth) > next_col >= 0:
+                if self.__labyrinth[next_row][next_col] == '_':
+                    self.__row_position += add_row
+                    self.__col_position += add_col
+            # todo: what happens when player steps on exit (field 'S')?
 
     def __convert_position(self, row_position: int = None, col_position: int = None) -> int:
         """
@@ -33,9 +55,9 @@ class Player:
         :return: Converted position
         """
         if row_position is None or col_position is None:
-            return self.row_position * len(self.labyrinth[0]) + self.col_position
+            return self.__row_position * len(self.__labyrinth[0]) + self.__col_position
 
-        return row_position * len(self.labyrinth[0]) + col_position
+        return row_position * len(self.__labyrinth[0]) + col_position
 
     def __predecessors_list(self) -> Dict[int, Union[int, List[int]]]:
         """
@@ -46,8 +68,8 @@ class Player:
         """
         predecessors = {}
         states = {}
-        for row in range(0, len(self.labyrinth)):
-            for col in range(0, len(self.labyrinth[0])):
+        for row in range(0, len(self.__labyrinth)):
+            for col in range(0, len(self.__labyrinth[0])):
                 predecessors[self.__convert_position(row, col)] = -1
                 states[self.__convert_position(row, col)] = False
 
@@ -56,8 +78,8 @@ class Player:
         row_move = [0, 0, -1, 1]
         col_move = [1, -1, 0, 0]
 
-        row_queue = [self.row_position]
-        col_queue = [self.col_position]
+        row_queue = [self.__row_position]
+        col_queue = [self.__col_position]
 
         while row_queue:
             current_row = row_queue.pop(0)
@@ -67,15 +89,15 @@ class Player:
                 next_row = current_row + row_move[i]
                 next_col = current_col + col_move[i]
 
-                if self.labyrinth[next_row][next_col] == "#":
+                if self.__labyrinth[next_row][next_col] == "#":
                     continue
 
-                if states[next_row * len(self.labyrinth[0]) + next_col]:
+                if states[next_row * len(self.__labyrinth[0]) + next_col]:
                     continue
 
                 predecessors[self.__convert_position(next_row, next_col)] = [current_row, current_col]
 
-                if self.labyrinth[next_row][next_col] == "X":
+                if self.__labyrinth[next_row][next_col] == "X":
                     break
 
                 states[self.__convert_position(next_row, next_col)] = True
