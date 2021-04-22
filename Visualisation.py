f8d32609fa67ad57e248df488dbc87911ec131a7
jakubhi <50047839+jakubhi@users.noreply.github.com>
2021-04-22 15:06:39 +0200

    Update Visualisation.py
    edited comments

--- Visualisation.py
+++ Visualisation.py
@@ -2,41 +2,41 @@ from pygame.locals import *
 
 from Const import *
 
-# blk znamená blok
-# vykreslenie blokov som nastavil zatiaľ tak aby mali rovnakú veľkosť a šírku
-# v ľavej časti obrazovky nechávam miesto na vykreslovanie skóre, tlačítok ako hint, end, menu,...
+# blk stands for block
+# the height of each of the blocks is supposed to be the same as their width
+# on the left side of the screen is left space for future additions of buttons and other features
 
-# začiatočné súradnice ku ktorým sa ešte bude pridávať hodnota podľa toho v ktorom stĺpci a riadku sú
+# starting coordinates for columns and rows ( for the first block )
 X_BLK_START = 80
 Y_BLK_START = 0
 
-# počet blokov
+# number of blocks
 BLK_NUM = len(LABYRINTH[0])
 
-# veľkosť bloku
+# block size
 BLK_SIZE = WINDOW_HEIGHT // BLK_NUM
 
-# koniec bloku, znovu sa k tomu bude pridávať hodnota podľa toho kde sa nachádzajú v riadku a stĺpci
-# tieto hodnoty sú vlastne ako keby pre prvý blok
+# end of the block, depending on their row and column position
+# this one is for the end of the first block
 X_BLK_END = X_BLK_START + BLK_SIZE
 Y_BLK_END = Y_BLK_START + BLK_SIZE
 
 
 def draw_labyrinth():
-    # bloky sa vykreslujú po riadkoch, nie po stĺpcoch
+    # blocks are being drawn in rows, not columns
     for i in range(len(LABYRINTH[0])):
         for j in range(len(LABYRINTH[0])):
             rect = pygame.Rect(X_BLK_START + (BLK_SIZE * j), Y_BLK_START + (BLK_SIZE * i), BLK_SIZE - 2, BLK_SIZE - 2)
-            # stena
+            # wall
             if MAP1[i][j] == '#':
                 pygame.draw.rect(DISPLAY_SURFACE, BROWN, rect)
-                # - 2 vo výpočte zabezpečuje medzery medzi blokmi, aby boli ľahšie rozoznateľné
-                # prvá čiara je z horného ľavého rohu do stredu pravej strany
-                # druhá je zo stredu pravej strany do ľavého doľného rohu
-                # tretia z horného pravého rohu do stredu ľavej strany
-                # štvrtá zo stredu ľavej strany do pravého doľného rohu
-                # X_COORDS je X-ová súradnica lavej steny daného bloku
-                # Y_coords je Y-ová súradnica hornej steny daného bloku
+                # - 2 -> used to make space between blocks
+                # first line goes from top left corner to the middle of right side of the block
+                # second line goes from the middle of the right side to the bottom left corner
+                # third line goes from top right corner to the middle of the left side
+                # fourth line goes from the middle of the left side to the bottom right corner
+                # X_COORDS is X coordinate of left side of the particular block
+                # Y_COORDS is Y coordinate of top side of the particular block
                 X_COORDS = X_BLK_START + (BLK_SIZE * j)
                 Y_COORDS = Y_BLK_START + (BLK_SIZE * i)
 
@@ -48,10 +48,10 @@ def draw_labyrinth():
                                  (X_COORDS, Y_COORDS + (BLK_SIZE // 2) - 2), 1)
                 pygame.draw.line(DISPLAY_SURFACE, BLACK, (X_COORDS, Y_COORDS + (BLK_SIZE // 2)),
                                  (X_COORDS + BLK_SIZE - 2, Y_COORDS + BLK_SIZE - 2), 1)
-            # priechod
+            # road
             if MAP1[i][j] == '_':
                 pygame.draw.rect(DISPLAY_SURFACE, GREEN, rect)
-            # Začiatok a koniec
+            # start (player spawn) and the end
             if MAP1[i][j] == 'X' or MAP1[i][j] == 'S':
                 pygame.draw.rect(DISPLAY_SURFACE, BLUE, rect)
                 if MAP1[i][j] == 'X':
